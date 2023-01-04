09cbec944f64c377093a23274b6006c844f2d3e1
Hanbit-Kang <fchopinof99@gmail.com>
2023-01-04 16:46:01 +0900

    Add FiveStarsView module

---
+++ fivestarsview/src/main/java/com/abit/fivestarsview/FiveStarsView.kt
@@ -0,0 +1,14 @@
+package com.abit.fivestarsview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class FiveStarsView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : FrameLayout(context, attrs, defStyle) {
+
+
+}
