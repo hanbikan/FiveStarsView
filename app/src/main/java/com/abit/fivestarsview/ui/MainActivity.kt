3d6d31412a1c31c5557d37addad77e09565ed7d4
Hanbit-Kang <fchopinof99@gmail.com>
2023-01-06 12:37:54 +0900

    Implement jetpack navigation

---
+++ app/src/main/java/com/abit/fivestarsview/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.abit.fivestarsview.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abit.fivestarsview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
